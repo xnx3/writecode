5882acba748678d2813d4bde7d1c09a44014a904
xnx3 <mail@xnx3.com>
2022-09-21 15:59:26 +0800

    update springboot

--- springboot/src/main/java/com/xnx3/writecode/springboot/Demo.java
+++ springboot/src/main/java/com/xnx3/writecode/springboot/Demo.java
@@ -8,21 +8,22 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.stereotype.Component;
-
 import com.xnx3.j2ee.util.ApplicationPropertiesUtil;
+import com.xnx3.writecode.WriteCode;
 import com.xnx3.writecode.interfaces.impl.Mysql;
 
-public class WriteCode {
+public class Demo {
 	
 	public static void main(String[] args) {
-		
-		String host = properties.getProperty("database.ip");	//主机，可以填写域名或ip
+		String host = "local.mysql.leimingyun.com";	//主机，可以填写域名或ip
 		int port = 3306;			//端口号
-		String databaseName = properties.getProperty("database.name"); //数据表的名字
-		String username = properties.getProperty("spring.datasource.username"); 	//数据库登录用户名
-		String password = properties.getProperty("spring.datasource.password");;	//数据库登录密码
+		String databaseName = "wangmarket"; //数据表的名字
+		String username = "root"; 	//数据库登录用户名
+		String password = "111111";	//数据库登录密码
 		
-		com.xnx3.writecode.WriteCode code = new com.xnx3.writecode.WriteCode(new Mysql(host, port, databaseName, username, password));
+		WriteCode code = new WriteCode(new Mysql(host, port, databaseName, username, password));
+		//手动设置当前生成的包名。如果不手动设置，那生成的entity实体类就会在当前包下
+		//code.setJavaPackage("com.xnx3.entity");	
 		code.writeEntityCodeBySelectTableUI();
 	}
 }
