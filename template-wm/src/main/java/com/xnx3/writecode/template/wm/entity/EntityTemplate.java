a4e8c9db2f5130445436ef1d8e1c185732f29dca
xnx3 <mail@xnx3.com>
2022-09-22 10:05:52 +0800

    优化 template-wm

--- template-wm/src/main/java/com/xnx3/writecode/template/wm/entity/EntityTemplate.java
+++ template-wm/src/main/java/com/xnx3/writecode/template/wm/entity/EntityTemplate.java
@@ -2,11 +2,14 @@ package com.xnx3.writecode.entity;
 
 import com.xnx3.writecode.bean.Template;
 
+/**
+ * 生成wm中实体类的默认模板
+ * @author 管雷鸣
+ *
+ */
 public class EntityTemplate extends Template{
 	public EntityTemplate() {
-//		template.setJavaPackage(null);
 		this.setTemplateFileName("entity.template");
-		this.setWriteFileAbsolutePath(null);
 		this.setWriteFileName("{database.table.name.hump.upper}.java");
 	}
 }
