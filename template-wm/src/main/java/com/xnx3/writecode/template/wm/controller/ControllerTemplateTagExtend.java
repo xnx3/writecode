7cb9157fdf3ca645b459f2a1a64e71f176217129
yuanzhangaly <1258704490@qq.com>
2022-09-27 10:22:40 +0800

    移除无用的拓展

--- template-wm/src/main/java/com/xnx3/writecode/template/wm/controller/ControllerTemplateTagExtend.java
+++ template-wm/src/main/java/com/xnx3/writecode/template/wm/controller/ControllerTemplateTagExtend.java
@@ -12,8 +12,6 @@ import com.xnx3.writecode.util.TemplateUtil;
 public class ControllerTemplateTagExtend implements TemplateTagExtendInterface {
 	
 	private String projectUrlPath;	//生成的controller的访问请求url的路径是什么，传入格式入 /user/api/  这个其实就是Controller上的@RequestMapping用的，使用方式如： @RequestMapping("{project.url.path}{database.table.name.hump.upper}/")
-	private String projectEntityPath;	//生成的实体类存放的位置
-	private String projectVoPath;	//生成的vo存放的位置
 	
 	public String getProjectUrlPath() {
 		return projectUrlPath;
@@ -21,25 +19,11 @@ public class ControllerTemplateTagExtend implements TemplateTagExtendInterface {
 	public void setProjectUrlPath(String projectUrlPath) {
 		this.projectUrlPath = projectUrlPath;
 	}
-	public String getProjectEntityPath() {
-		return projectEntityPath;
-	}
-	public void setProjectEntityPath(String projectEntityPath) {
-		this.projectEntityPath = projectEntityPath;
-	}
-	public String getProjectVoPath() {
-		return projectVoPath;
-	}
-	public void setProjectVoPath(String projectVoPath) {
-		this.projectVoPath = projectVoPath;
-	}
 	
 	@Override
 	public String appendTag(String text, TemplateUtil templateUtil, TableBean tableBean) {
 		
 		text = TemplateUtil.replaceAll(text, "{project.url.path}", this.projectUrlPath);
-		text = TemplateUtil.replaceAll(text, "{project.entity.path}", this.projectEntityPath);
-		text = TemplateUtil.replaceAll(text, "{project.vo.path}", this.projectVoPath);
 		
 		return text;
 	}
