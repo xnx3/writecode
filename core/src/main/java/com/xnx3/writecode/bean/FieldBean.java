06b70a68679d1a4e3cbbf29728e0de3ca703f891
xnx3 <mail@xnx3.com>
2022-10-27 12:49:23 +0800

    如果字段没有设置注释，那么将返回字段名字

--- core/src/main/java/com/xnx3/writecode/bean/FieldBean.java
+++ core/src/main/java/com/xnx3/writecode/bean/FieldBean.java
@@ -6,7 +6,7 @@ package com.xnx3.writecode.bean;
  */
 public class FieldBean {
 	private String name;			//{database.table.field.name} 数据表中，字段名。如 role_id
-	private String comment;			//{database.table.field.comment} 数据表中，字段的注释
+	private String comment;			//{database.table.field.comment} 数据表中，字段的注释。如果字段没有设置注释，那么将返回字段名字
 	private String defaultvalue;	//{database.table.field.default} 数据表中，字段的默认值。如果数据表中字段本身并没有设置，为null，那这里就会输出空字符串
 	private String datatype;		//{database.table.field.datatype} 数据表中，字段的数据类型，如 int、char、varchar 等
 	private String length;			//{database.table.field.length} 数据表中，字段数据的长度，比如类型是int、chat等类型，这里输出格式为 11 ,比如类型是float型，这里输出格式为 3,6
@@ -27,6 +27,9 @@ public class FieldBean {
 		this.name = name;
 	}
 	public String getComment() {
+		if(this.comment == null || this.comment.length() == 0) {
+			return this.name;
+		}
 		return comment;
 	}
 	public void setComment(String comment) {
